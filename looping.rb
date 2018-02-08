965b5eedb72302b00be68ca4d35600cbf88f747f
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-08 18:16:30 +0000

    Done.

--- looping.rb
+++ looping.rb
@@ -1,5 +1,16 @@
 def looping
-	#your code here
+
+counter = 0
+
+  loop do
+    counter += 1
+    puts "#{counter} Wingardium Leviosa"
+
+  if counter >= 10
+    break
+    end
+  end
 end
 
 #call your method here
+looping
